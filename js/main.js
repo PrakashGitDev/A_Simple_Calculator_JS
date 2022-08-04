9d8cf5f912013a9f2033ad69db1c8b5f54b012cc
Prakash <pmandal748@gmsil.com>
2022-08-04 08:48:30 +0545

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var input=document.getElementById("input"); //input and output panel
+var operator=document.querySelectorAll(".operators"); //operator buttons
+var number=document.querySelectorAll(".numbers"); //number buttons
+//console.log(number);
+var result=document.getElementById("result"); //equal button
+var clear=document.getElementById("clear"); //clear button
+resultDisplayed=false; //flag to keep an eye on what output is displayed
+//adding click handlers event
+for(var i=0;i<number.length;i++){
+number[i].addEventListener("click",function(e){
+    
+})
+}
+
